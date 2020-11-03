fa667cd8688c05f19f6097a12d22cab93e074e82
AnandaAryaPratama <nandarz0807@gmail.com>
2020-11-03 11:23:09 +0700

    Update sql

--- uts/portofolio.sql
+++ uts/portofolio.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Nov 2020 pada 13.43
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 7.3.3
+-- Waktu pembuatan: 03 Nov 2020 pada 05.22
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -56,15 +55,16 @@ CREATE TABLE `biodata` (
   `jenis_kelamin` varchar(15) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `agama` varchar(15) NOT NULL,
-  `kewarganegaraan` varchar(20) NOT NULL
+  `kewarganegaraan` varchar(20) NOT NULL,
+  `kegiatan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `biodata`
 --
 
-INSERT INTO `biodata` (`id_biodata`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `agama`, `kewarganegaraan`) VALUES
-(1, 'Ananda Arya Pratama', 'Jakarta', '08 Juli 2000', 'Laki - Laki', 'Villa Mutiara Serpong', 'Islam', 'Indonesia');
+INSERT INTO `biodata` (`id_biodata`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `agama`, `kewarganegaraan`, `kegiatan`) VALUES
+(1, 'Ananda Arya Pratama', 'Jakarta', '08 Juli 2000', 'Laki - Laki', 'Villa Mutiara Serpong', 'Islam', 'Indonesia', 'Graphic Designer, Technology/IT, Web Developer, Bendahara HIMA, Programmer');
 
 -- --------------------------------------------------------
 
@@ -145,6 +145,28 @@ INSERT INTO `pendidikan` (`id_biodata`, `id_pendidikan`, `tahun_sd`, `nama_sd`, 
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `penelitian`
+--
+
+CREATE TABLE `penelitian` (
+  `id_biodata` int(10) NOT NULL,
+  `id_penelitian` int(10) NOT NULL,
+  `keterangan` text NOT NULL,
+  `kode` varchar(25) NOT NULL,
+  `tahun` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penelitian`
+--
+
+INSERT INTO `penelitian` (`id_biodata`, `id_penelitian`, `keterangan`, `kode`, `tahun`) VALUES
+(1, 1, 'Pencatatan Ciptaan Kemenkumham Perancangan Desain Website Online Shop', 'EC00201940434', '16 Mei 2019'),
+(1, 5, 'Pencatatan Ciptaan Kemenkumham Aplikasi JSDPKU 2.0', 'EC00202015943', '20 Mei 2020');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `pengalaman`
 --
 
@@ -152,7 +174,7 @@ CREATE TABLE `pengalaman` (
   `id_biodata` int(10) NOT NULL,
   `id_pengalaman` int(10) NOT NULL,
   `foto_kerja` varchar(25) NOT NULL,
-  `nama_kerja` varchar(20) NOT NULL,
+  `nama_kerja` varchar(100) NOT NULL,
   `tahun_kerja` varchar(100) NOT NULL,
   `deskripsi_kerja` text NOT NULL,
   `foto_panitia` varchar(25) NOT NULL,
@@ -165,7 +187,8 @@ CREATE TABLE `pengalaman` (
 --
 
 INSERT INTO `pengalaman` (`id_biodata`, `id_pengalaman`, `foto_kerja`, `nama_kerja`, `tahun_kerja`, `deskripsi_kerja`, `foto_panitia`, `nama_acara`, `deskripsi_panitia`) VALUES
-(1, 1, 'pizzahut.jpg', 'Magang Pizza Hut', 'July 2019 - September 2019', 'Bekerja di sebuah restoran Pizza Hut Alam Sutera sebagai Server', 'forka.jpg', 'Forka Fest 2019', 'Menjadi Panitia dalam acara Forka Fest 2019 sebagai Panitia Perlengkapan');
+(1, 1, 'pizzahut.png', 'Magang Pizza Hut', 'July 2019 - September 2019', 'Bekerja di sebuah restoran Pizza Hut Alam Sutera sebagai Server', 'forka.jpg', 'Forka Fest 2019', 'Menjadi Panitia dalam acara Forka Fest 2019 sebagai Panitia Perlengkapan'),
+(1, 3, 'upj.png', 'Magang Universitas Pembangunan Jaya', 'Januari 2019 - Desember 2019', 'Sebagai fasilitator JSDP Fakultas Teknologi dan Desain', 'SP.jpg', 'Sumpah Pemuda 2020', 'Menjadi Panitia dalam acara Sumpah Pemuda 2020 sebagai Bendahara');
 
 -- --------------------------------------------------------
 
@@ -226,6 +249,13 @@ ALTER TABLE `pendidikan`
   ADD KEY `id_biodata` (`id_biodata`);
 
 --
+-- Indeks untuk tabel `penelitian`
+--
+ALTER TABLE `penelitian`
+  ADD PRIMARY KEY (`id_penelitian`),
+  ADD KEY `id_biodata` (`id_biodata`);
+
+--
 -- Indeks untuk tabel `pengalaman`
 --
 ALTER TABLE `pengalaman`
@@ -274,10 +304,16 @@ ALTER TABLE `pendidikan`
   MODIFY `id_pendidikan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT untuk tabel `penelitian`
+--
+ALTER TABLE `penelitian`
+  MODIFY `id_penelitian` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT untuk tabel `pengalaman`
 --
 ALTER TABLE `pengalaman`
-  MODIFY `id_pengalaman` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pengalaman` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `sosmed`
@@ -300,6 +336,12 @@ ALTER TABLE `kemampuan`
 --
 ALTER TABLE `pendidikan`
   ADD CONSTRAINT `pendidikan_ibfk_1` FOREIGN KEY (`id_biodata`) REFERENCES `biodata` (`id_biodata`);
+
+--
+-- Ketidakleluasaan untuk tabel `penelitian`
+--
+ALTER TABLE `penelitian`
+  ADD CONSTRAINT `penelitian_ibfk_1` FOREIGN KEY (`id_biodata`) REFERENCES `biodata` (`id_biodata`);
 
 --
 -- Ketidakleluasaan untuk tabel `pengalaman`
